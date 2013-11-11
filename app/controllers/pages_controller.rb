34e89312dc54882c0f897c74214c9c2e206d669b
jbsaillan <jbsaillan@yahoo.fr>
2013-11-11 16:49:04 +0100

    Fin d ela modification des pages statiques contact & home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def home
+  	@titre = 'Accueil'
+  	@sous_titre = 'Voici l\'index du site'
   end
 
   def contact
+  	@titre = 'Contact'
+  	@sous_titre = 'A partir de cette page vous aurez la possibiltée de nous contacter'
   end
 end
