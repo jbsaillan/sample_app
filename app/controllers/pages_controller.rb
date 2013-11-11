480c539dabf02c402efb20d5e0e03829c815d120
jbsaillan <jbsaillan@yahoo.fr>
2013-11-11 16:55:51 +0100

    Ajout de la page aide

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,9 @@ class PagesController < ApplicationController
   	@titre = 'Contact'
   	@sous_titre = 'A partir de cette page vous aurez la possibiltée de nous contacter'
   end
+
+  def aide
+  	@titre = 'Aide'
+  	@sous_titre = 'Vous voici sur une page pouvant vous fournir toute l\'aide nécessaire'
+  end
 end
