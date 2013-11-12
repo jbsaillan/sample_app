8123b2fd597effafce1a0c1caed23cc3be473ad9
jbsaillan <jbsaillan@yahoo.fr>
2013-11-12 16:16:15 +0100

    Amélioration de l'helper et ajout du CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # Retourner un titre basé sur la page.
   def titre
-    base_titre = "Simple App du Tutoriel Ruby on Rails"
-    if @titre.nil?
-      base_titre
-    else
-      "#{base_titre} | #{@titre}"
-    end
+    base_titre = "Vous etes sur la page :"
+    base_titre2 = "Vous ete sur une page"
+
+    return base_titre2 if @titre.nil?
+    return "#{base_titre} #{@titre}" 
   end
+
 end
